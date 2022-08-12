a12ebf90d192a6e62d774567bce3c157677acd97
Guilherme da Rocha Spanserki <guilherme.spanserki@cas.ilumisol.com.br>
2022-08-12 09:57:58 -0300

    Projeto finalizado

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,29 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    const newTask = {
+      id: Math.floor(Math.random() * (100 -1)),
+      title: newTaskTitle,
+      isComplete: false,
+    }
+
+    setTasks(previusState =>  [...previusState, newTask])
+    setNewTaskTitle('')
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const alterar = tasks.map(task => task.id === id ? {
+      ...task, isComplete: !task.isComplete
+    } : task );
+    
+    setTasks(alterar)
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const remover = tasks.filter(data => data.id != id)
+    setTasks(remover)
   }
 
   return (
